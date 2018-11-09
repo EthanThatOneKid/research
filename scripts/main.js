84f913b1bafe66f3d0110a903c32ef0347727103
Ethan <ethan.r.davidson@gmail.com>
2018-11-08 22:11:32 -0800

    finished project cards

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,14 @@
 const create_project_cards = (qsel, data) => {
 
   const cards_container = data.reduce((acc, card_data) => {
+    const li = document.createElement("li");
+    li.style.margin = "10px";
     const card = new Project_Card(card_data);
-    acc.appendChild(card.el);
+    li.appendChild(card.el);
+    acc.appendChild(li)
     return acc;
-  }, document.createElement("div"));
+  }, document.createElement("ul"));
+  cards_container.classList.add("cards_container");
 
   document.querySelector(qsel).innerHTML = cards_container.outerHTML;
 
--- scripts/project_card.js
+++ scripts/project_card.js
@@ -1,37 +1,12 @@
 // https://codepen.io/uxjulia/pen/xgjmzp
 
-// check out some badges to use on https://shields.io/#/examples/build
-
-/*
-
-div
-  img
-  content
-    frameworks (stack)
-    project name
-    website
-    GitHub link
-    (more info)
-      project name
-      status
-      description
-
-example_data = {
-    "project": "Project X",
-    "stack": ["react", "express", "node"],
-    "description": "Some sample text giving project elevator pitch and blah blah and other stuff and goals and dreams and rainbows and butterflies and sunshine. ",
-    "website": "https://www.google.com",
-    "github": "https://www.github.com",
-    "image": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/326643/Sample%20Logo.png",
-    "status": "In Development"
-  };
-
-*/
+// check out some badges to use on https://shields.io/#/examples/
 
 
 class Project_Card {
 
   constructor(data) {
+    this.data = data;
     this.el = this.createElement();
   }
 
@@ -46,55 +21,53 @@ class Project_Card {
     photo.style.backgroundImage = `url(cgi-bin/${this.data.image})`;
     photo.className = "photo";
 
-    const stack = this.data.stack.reduce((ul, cur) => {
-      const li = document.createElement("li");
-      li.className = "author";
-
-      const span = document.createElement("span");
-
-      span.textContent = cur;
-
-      li.appendChild(span);
-      ul.appendChild(li);
-
-      return ul;
-    }, document.createElement("ul"));
-    stack.className = "details";
-
-    const details = document.createElement("li");
-    details.className = "author";
-    details.textContent = this.data.description;
-    stack.appendChild(details);
-    
-    const website = document.createElement("li");
-    const website_a = document.createElement("a");
-    website.className = "author";
-    website_a.href = this.data.website;
-    website_a.textContent = "website";
-    website.appendChild(website_a);
-    stack.appendChild(website);
-    
-    const github_repo = document.createElement("li");
-    const github_repo_a = document.createElement("a");
-    github_repo.className = "author";
-    github_repo_a.href = this.data.github;
-    github_repo_a.textContent = "github";
-    github_repo.appendChild(github_repo_a);
-    stack.appendChild(github_repo);
-    
-    const status = document.createElement("li");
-    status.className = "author";
-    status.textContent = `<b>status</b>: ${this.data.status}`;
-    stack.appendChild(status);
-
     const description = document.createElement("div");
     description.className = "description";
 
     const h1 = document.createElement("h1");
-    h1.innerText = this.data.project;
+    h1.textContent = this.data.project;
 
     const p = document.createElement("p");
-    p.innerText = this.data.description;
+    p.textContent = this.data.description;
+
+    const details = document.createElement("ul");
+    details.className = "details";
+
+    const stack = this.data.stack.reduce((li, cur) => {
+      const span = document.createElement("span");
+      span.textContent = cur;
+      span.className = "stack";
+      li.appendChild(span);
+      return li;
+    }, document.createElement("li"));
+    details.appendChild(stack);
+
+    const website = document.createElement("li");
+    const website_a = document.createElement("a");
+    website.classList.add("author", "ext_link");
+    website_a.href = this.data.website;
+    website_a.textContent = "website";
+    website.appendChild(website_a);
+    details.appendChild(website);
+
+    const github_repo = document.createElement("li");
+    const github_repo_a = document.createElement("a");
+    github_repo.classList.add("author", "ext_link");
+    github_repo_a.href = this.data.github;
+    github_repo_a.textContent = "github";
+    github_repo.appendChild(github_repo_a);
+    details.appendChild(github_repo);
+
+    const status = document.createElement("li");
+    status.classList.add("author", "status");
+    status.innerHTML = `<b>status</b>: ${this.data.status}`;
+    details.appendChild(status);
+
+    const readmore = document.createElement("a");
+    readmore.classList.add("readmore");
+    readmore.textContent = "read more";
+    readmore.href = this.data.readmore;
+    readmore.title = `Read more about ${this.data.project}...`;
 
     description.appendChild(h1);
     description.appendChild(p);
@@ -104,6 +77,7 @@ class Project_Card {
 
     card.appendChild(meta);
     card.appendChild(description);
+    card.appendChild(readmore);
 
     return card;
   }
